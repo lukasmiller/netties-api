1546ece8296be257db15560650832bd418c2b222
Luke Miller <lukas.miller@live.com>
2022-03-04 12:30:08 -0600

    Added  runtime

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,14 @@ module.exports = {
   apps : [
     {
       name: "server",
-      script: 'dist/index.js'
+      script: 'dist/index.js',
+      instances: "max",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
       // ,
       // instances: "max",
       // exec_mode: "cluster"
