dd69743dc31dedc4e80a79e14be335f4081ac5e8
Luke Miller <lukas.miller@live.com>
2022-03-04 12:42:04 -0600

    Added in worker app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,16 +10,17 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
       }
-      // ,
-      // instances: "max",
-      // exec_mode: "cluster"
+    },
+    {
+      name: "worker",
+      script: 'dist/index-worker.js',
+      instances: 1,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
     }
-    // ,
-    // {
-    //   name: "worker",
-    //   script: 'dist/index-worker.js',
-    //   instances: "max",
-    //   exec_mode: "cluster"
-    // }
   ]
 };
