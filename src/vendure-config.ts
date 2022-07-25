e2e66bb97c6944300dbb9e9134c03a70dceb0dfb
Luke Miller <lukas.miller@live.com>
2022-07-25 14:17:58 -0500

    Adding files

---
+++ src/vendure-config.ts
@@ -0,0 +1,80 @@
+import {
+    dummyPaymentHandler,
+    DefaultJobQueuePlugin,
+    DefaultSearchPlugin,
+    VendureConfig,
+} from '@vendure/core'; 
+import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin';
+import { AssetServerPlugin } from '@vendure/asset-server-plugin';
+import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
+import path from 'path';
+
+export const config: VendureConfig = {
+    apiOptions: {
+        port: 3000,
+        adminApiPath: 'admin-api',
+        adminApiPlayground: {
+            settings: {
+                'request.credentials': 'include',
+            } as any,
+        },// turn this off for production
+        adminApiDebug: true, // turn this off for production
+        shopApiPath: 'shop-api',
+        shopApiPlayground: {
+            settings: {
+                'request.credentials': 'include',
+            } as any,
+        },// turn this off for production
+        shopApiDebug: true,// turn this off for production
+    },
+    authOptions: {
+        superadminCredentials: {
+            identifier: 'nettie',
+            password: 'Oldpaths1!',
+        },
+        cookieOptions: {
+          secret: process.env.COOKIE_SECRET || 'cookie-secret',
+        },
+    },
+    dbConnectionOptions: {
+        type: 'postgres',
+        synchronize: true,
+        logging: false,
+        url: 'postgres://rgkkwvhrdcskfq:2ffc3503f757b49ecf904b4c259a08e02a378a338fa9b6d558b4e7858691d52e@ec2-44-206-214-233.compute-1.amazonaws.com:5432/df78s0q5pjgtha',
+        ssl: 
+        {
+            rejectUnauthorized: false
+        },
+        migrations: [path.join(__dirname, '../migrations/*.ts')],
+    },
+    paymentOptions: {
+        paymentMethodHandlers: [dummyPaymentHandler],
+    },
+    customFields: {},
+    plugins: [
+        AssetServerPlugin.init({
+            route: 'assets',
+            assetUploadDir: path.join(__dirname, '../static/assets'),
+        }),
+        DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
+        DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
+        EmailPlugin.init({
+            devMode: true,
+            outputPath: path.join(__dirname, '../static/email/test-emails'),
+            route: 'mailbox',
+            handlers: defaultEmailHandlers,
+            templatePath: path.join(__dirname, '../static/email/templates'),
+            globalTemplateVars: {
+                // The following variables will change depending on your storefront implementation
+                fromAddress: '"example" <noreply@example.com>',
+                verifyEmailAddressUrl: 'http://localhost:8080/verify',
+                passwordResetUrl: 'http://localhost:8080/password-reset',
+                changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
+            },
+        }),
+        AdminUiPlugin.init({
+            route: 'admin',
+            port: 3002,
+        }),
+    ],
+};
